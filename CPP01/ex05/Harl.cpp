26bfd86ec860ea58d4a4ef4c7d03f5a9f230cb53
Cléo Le Tron <cle-tron@car11s1.42barcelona.com>
2024-11-22 14:33:00 +0100

    CPP01/ex05 ok switch statement and pointer to member function

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -6,9 +6,67 @@
 /*   By: cle-tron <cle-tron@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 16:11:40 by cle-tron          #+#    #+#             */
-/*   Updated: 2024/11/21 16:20:02 by cle-tron         ###   ########.fr       */
+/*   Updated: 2024/11/22 14:29:17 by cle-tron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+#include <iostream>
+#include <string>
 
+Harl::Harl( void ) { }
+
+Harl::~Harl( void ) { }
+
+void	Harl::debug( void ) {
+	std::cout << "debug level 1" << std::endl;
+}
+
+void	Harl::info( void ) {
+	std::cout << "info level 2" << std::endl;
+}
+
+void	Harl::warning( void ) {
+	std::cout << "warning level 3" << std::endl;
+}
+
+void	Harl::error( void ) {
+	std::cout << "error level 4" << std::endl;
+}
+
+void	Harl::setLevel( std::string level ) {
+	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	
+	for (int i = 0; i < 4; i++) {
+		if (level == levels[i]) {
+			this->level = i + 1;
+			return;
+		}
+	}
+	this->level = 0;
+//	std::cout << "Invalid level, try again!" << std::endl;
+}
+
+void	Harl::complain( std::string level ) {
+	void	(Harl::*f)(void) = NULL;
+
+	setLevel(level);
+	switch (this->level) {
+		case DEBUG:
+			f = &Harl::debug;
+			break;
+		case INFO:
+			f = &Harl::info;
+			break;
+		case WARNING:
+			f = &Harl::warning;
+			break;
+		case ERROR:
+			f = &Harl::error;
+			break;
+		default:
+			std::cout << "Invalid level, try again!" << std::endl;
+			return;
+	}
+	(this->*f)();
+}
--- CPP01/ex05/Harl.hpp
+++ CPP01/ex05/Harl.hpp
@@ -6,12 +6,19 @@
 /*   By: cle-tron <cle-tron@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 16:11:34 by cle-tron          #+#    #+#             */
-/*   Updated: 2024/11/21 16:20:02 by cle-tron         ###   ########.fr       */
+/*   Updated: 2024/11/22 14:29:29 by cle-tron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HARL_HPP
 #define HARL_HPP
+
+#define DEBUG	1
+#define INFO	2
+#define WARNING	3
+#define ERROR	4
+
+#include <string>
 
 class	Harl {
 private:
@@ -19,7 +26,14 @@ private:
 	void	info( void );
 	void	warning( void );
 	void	error( void );
+	int		level;
+	void	setLevel( std::string level );
+//	void	*f(void);	
 
 public:
-	void complain( std::string level );
+	Harl( void );
+	~Harl( void );
+	void	complain( std::string level );
 };
+
+#endif
