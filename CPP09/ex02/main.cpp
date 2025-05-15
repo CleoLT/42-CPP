a1c6254c90cef354cfe8ca0519e691f207d28cee
Cléo Le Tron <cle-tron@cbr1s2.42barcelona.com>
2025-05-15 18:46:59 +0200

    CPP09/ex02 Makefile

---
+++ CPP09/ex02/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cle-tron <cle-tron@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15 17:40:39 by cle-tron          #+#    #+#             */
+/*   Updated: 2025/05/15 18:46:27 by cle-tron         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+#include <iostream>
+
+int main( int argc, char * argv[] ) {
+	if ( argc < 3 ) {
+		std::cout << "You must enter at least 2 numbers" << std::endl;
+		return 1;
+	}
+
+
+
+
+	return 0;
+}
+		
